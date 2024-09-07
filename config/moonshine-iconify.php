62363fff286ba91cef30ab904e9c12f779fcb061
Murchenko <rabix346@yandex.ru>
2024-09-07 10:07:03 +0300

    fix: restore config

--- config/moonshine-iconify.php
+++ config/moonshine-iconify.php
@@ -1,5 +1,14 @@
 <?php
 
 return [
+    /**
+     * When NULL - load iconify from CDN.
+     */
+    'iconify_script_url' => null,
 
+
+    /**
+     * Size for moonshine icons multiplied by this multiplier
+     */
+    'icon_size_multiplier' => 1,
 ];
